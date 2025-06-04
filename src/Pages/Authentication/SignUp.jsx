f2592237cd8b91cfe21dadcbf20a77091871ff7d
sifat tarafder <tmdsifat98@gmail.com>
2025-06-04 12:10:18 +0600

    sign up firebase config added

--- src/Pages/Authentication/SignUp.jsx
+++ src/Pages/Authentication/SignUp.jsx
@@ -1,11 +1,69 @@
 import React, { useState } from "react";
 import Button from "../../Components/Button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { FcGoogle } from "react-icons/fc";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import useAuth from "../../hooks/useAuth";
+import { updateProfile } from "firebase/auth";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
+  // contexts from contextProviders
+  const { createUser } = useAuth();
+
+  const navigate = useNavigate()
+
   const [showPass, setShowPass] = useState(false);
+  const [error, setError] = useState("");
+
+  //   password validation
+  const validatePassword = (e) => {
+    const value = e.target.value;
+    let errorMsg = "";
+    if (!/[a-z]/.test(value)) {
+      errorMsg = "Must have a lowercase letter";
+    } else if (!/[A-Z]/.test(value)) {
+      errorMsg = "Must have an uppercase letter";
+    } else if (value.length < 6) {
+      errorMsg = "Length must be at least 6 characters";
+    }
+    setError(errorMsg);
+  };
+
+  //sign up form handle
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formdata = new FormData(form);
+    const { name, password, email, photo } = Object.fromEntries(
+      formdata.entries()
+    );
+    createUser(email, password)
+      .then((res) => {
+        const user = res.user;
+        updateProfile(user, {
+          displayName: name,
+          photoURL: photo,
+        }).then(() => {
+          navigate("/");
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Account has been created successfully!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          setShowPass(false);
+        });
+      })
+      .catch((err) => {
+        Swal.fire({
+          title: `${err.message}`,
+          icon: "error",
+          draggable: true,
+        });
+      });
+  };
   return (
     <div className="relative md:w-11/12 mx-auto min-h-[calc(100vh-81px)] flex items-center justify-center bg-gradient-to-br from-[#ff3b58] to-[#02ebc4] dark:from-gray-900 dark:to-gray-700 overflow-hidden transition-colors duration-500">
       <div className="absolute top-[-100px] left-[-200px] w-[800px] h-[800px] bg-[#02ebc4] opacity-30 dark:bg-gray-700 rounded-full rotate-[25deg]"></div>
@@ -16,7 +74,7 @@ const SignUp = () => {
           Please Sign Up!
         </h2>
 
-        <form className="space-y-3">
+        <form onSubmit={handleSignUp} className="space-y-2">
           <div>
             <label className="block mb-1 font-semibold text-gray-600 dark:text-gray-300">
               Name
@@ -25,6 +83,7 @@ const SignUp = () => {
               type="text"
               name="name"
               placeholder="Enter your name"
+              required
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none"
             />
           </div>
@@ -36,6 +95,7 @@ const SignUp = () => {
               type="text"
               name="photo"
               placeholder="Enter your Photo URL"
+              required
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none"
             />
           </div>
@@ -47,6 +107,7 @@ const SignUp = () => {
               type="email"
               name="email"
               placeholder="Enter your email"
+              required
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none "
             />
           </div>
@@ -56,10 +117,13 @@ const SignUp = () => {
             </label>
             <div className="w-full flex justify-between items-center px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none">
               <input
+                onChange={validatePassword}
                 name="password"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
                 type={showPass ? "password" : "text"}
                 placeholder="Enter your password"
                 className="flex-1 border-0 outline-0"
+                required
               />
               <button
                 type="button"
@@ -70,7 +134,7 @@ const SignUp = () => {
               </button>
             </div>
           </div>
-
+          <p className="text-red-600 text-sm">{error}</p>
           <Button label="Sign Up" width="w-full" />
         </form>
         <p className="mt-4">
@@ -79,7 +143,7 @@ const SignUp = () => {
             Login
           </Link>
         </p>
-         <div className="divider divider-secondary">Or</div>
+        <div className="divider divider-secondary">Or</div>
         <button
           // onClick={handleGoogle}
           className="btn w-full dark:bg-gray-800 dark:text-white border- border-gray-300 text-black mt-1 dark:border-none"
--- src/Providers/ContextProvider.jsx
+++ src/Providers/ContextProvider.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AuthContext from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/firebase.config";
 
 const ContextProvider = ({ children }) => {
-  const userInfo = { name: "mango" };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfo = { createUser };
   return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
 
