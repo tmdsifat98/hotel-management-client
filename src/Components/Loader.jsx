a1df169e8e1172b449ad46917f63abc8a23527d3
sifat tarafder <tmdsifat98@gmail.com>
2025-08-10 17:11:58 +0600

    added loading spinner with ldrs

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { HashLoader } from "react-spinners";
+import { Cardio } from "ldrs/react";
+import "ldrs/react/Cardio.css";
 
-const Loader = ({h}) => {
+const Loader = ({ h }) => {
   return (
+    // Default values shown
     <div className={`flex justify-center items-center ${h?"min-h-[calc(100vh-74px)]":"h-24"}`}>
-      <HashLoader color="#02ebc4" />
+      <Cardio size="50" stroke="4" speed="2" color="#ff3b58" />
     </div>
   );
 };
